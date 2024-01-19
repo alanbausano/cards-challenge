0bbdf509c24e49a7961bd0e8516c2f0b8fded767
Alan Bausano <alanbausano@MacBook-Pro-de-Alan.local>
2024-01-19 13:13:53 -0300

    deleted unused folders

--- src/validation/schema.ts
+++
@@ -1,8 +0,0 @@
-import * as Yup from 'yup'
-
-export const validationSchema = Yup.object({
-  id: Yup.number().required('Id is required'),
-  name: Yup.string().optional(),
-  description: Yup.string().optional(),
-  website: Yup.string().optional()
-})
